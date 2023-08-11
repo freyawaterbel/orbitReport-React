32bb605085e33af099786db5109b262f41137c1d
Freya <freyawaterbel@gmail.com>
2023-08-11 10:46:07 -0400

    assn 6 complete

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import './styling.css'
+
+const Banner = () => {
+    return (
+    <div>
+        <header>Orbit Report</header>
+        <p>Click the buttons to see the satellites of that orbit type</p>
+    </div>
+)
+};
+export default Banner;
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,8 +1,21 @@
-const Buttons = () => {
+import './styling.css'
+
+import satData from "./satData";
+
+const Buttons = (props) => {
+  let filterByType = props.filterByType;
+  let setSat = props.setSat;
+  let displaySats = props.displaySats;
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+    <div className="flex-container">
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        )
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
   );
 };
